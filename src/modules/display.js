d672d912837be0482bdfae7dcea118c052719b96
Your Name <legend.it.bk.ru>
2022-01-20 18:07:31 +0500

    Change element - display control

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,24 +1,24 @@
-export default class DisplayManager {
+export default class ControlDisplay {
     static reset = (element) => {
       element.innerHTML = '';
     };
   
     static displayTask = (parent, task) => {
-      const taskDiv = DisplayManager.createHtml(parent, 'div', 'task');
-      const taskInput = DisplayManager.createHtml(taskDiv, 'div', 'task_input');
+      const taskDiv = ControlDisplay.createHtml(parent, 'div', 'task');
+      const taskInput = ControlDisplay.createHtml(taskDiv, 'div', 'task_input');
   
-      const input = DisplayManager.createHtml(taskInput, 'input');
+      const input = ControlDisplay.createHtml(taskInput, 'input');
       input.setAttribute('type', 'checkbox');
       input.setAttribute('class', `task_${task.index}_checkbox`);
   
-      const inputSecond = DisplayManager.createHtml(taskInput, 'input');
+      const inputSecond = ControlDisplay.createHtml(taskInput, 'input');
       inputSecond.setAttribute('type', 'text');
       inputSecond.setAttribute('class', `task_${task.index} input_task`);
       inputSecond.dataset.id = task.index;
       inputSecond.value = task.description;
       inputSecond.id = `input_task_${task.index}`;
   
-      const i = DisplayManager.createHtml(taskDiv, 'i', 'fas fa-ellipsis-v');
+      const i = ControlDisplay.createHtml(taskDiv, 'i', 'fas fa-ellipsis-v');
       i.setAttribute('class', 'fas fa-trash');
       i.id = `task_${task.index}`;
       i.dataset.id = task.index;
