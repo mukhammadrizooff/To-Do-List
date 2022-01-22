ee3a094f083a7de3c37a910175ae809ea38ff120
Your Name <legend.it.bk.ru>
2022-01-22 06:51:32 +0500

    Fix linter errors

--- src/root.js
+++ src/root.js
@@ -1,7 +1,7 @@
 export default class List {
-    constructor(description, completed, index) {
-      this.description = description;
-      this.completed = completed;
-      this.index = index;
-    }
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
   }
+}
